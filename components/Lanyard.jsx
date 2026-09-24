97a8817a5e8ba7870a235959eff077b588683d86
rabbit-hash <radithaja45@gmail.com>
2026-09-24 12:19:09 +0700

    refactor: extract data into separate files and clean up code structure

--- components/Lanyard.jsx
+++ components/Lanyard.jsx
@@ -1,4 +1,4 @@
-/* eslint-disable react/no-unknown-property */
+ 
 'use client';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import { Canvas, extend, useFrame } from '@react-three/fiber';
